3135aa09fdfcadf5944bbb6edb75811e515d9aeb
priyam2106 <rajpriyam260@gmail.com>
2026-09-16 11:00:17 +0530

    Time: 4 ms (2.15%) | Memory: 12.9 MB (31.05%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>=target:
+                
+                return i
+                
+        return n
+        
